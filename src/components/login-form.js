f3516431a54044b9ae147a4f2c44a83ec7ea72a5
ramah <ramahrudayb@gmail.com>
2024-08-31 14:24:18 -0500

    fix autocomplete password

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -4,10 +4,9 @@ import Form from "react-bootstrap/Form";
 
 function LoginForm(props) {
   return (
-
     <Col xs={12} md={4} className="card shadow p-3 bg-white">
       <h3 className="text-dark mb-4">Login</h3>
-      <Form className="py-2">
+      <Form className="py-2" autoComplete="off">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
@@ -15,27 +14,30 @@ function LoginForm(props) {
             placeholder="Enter email"
             onChange={props.onEmailChange}
             value={props.email}
+            autoComplete="off"
           />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
+        <input type="text" style={{ display: 'none' }} autoComplete="username" />
 
         <Form.Group
           className="mb-3"
           controlId="formBasicPassword"
-          onChange={props.onPassWordChange}
-          value={props.password}
         >
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={props.onPassWordChange}
+            value={props.password}
+            autoComplete="new-password"
+            aria-autocomplete="none"  // Using "new-password" prevents auto-fill
+          />
         </Form.Group>
         <Button variant="primary" type="submit" onClick={props.onSubmit}>
           Submit
         </Button>
       </Form>
     </Col>
-
   );
 }
 
